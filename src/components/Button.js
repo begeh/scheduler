2f44943cbac2eb4a4196e69cb2635130cd8faccf
Bashir <b_egeh@hotmail.com>
2020-02-17 12:23:52 -0500

    also initial commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,14 @@ import React from "react";
 
 import "components/Button.scss";
 
+const classNames = require("classnames");
+
 export default function Button(props) {
-   return <></>;
+   const btnClass = classNames({
+      button: true,
+      'button--confirm': props.confirm,
+      'button--danger': props.danger
+   });
+
+   return <button onClick={props.onClick} disabled={props.disabled} className={btnClass}>{props.children}</button>;
 }
