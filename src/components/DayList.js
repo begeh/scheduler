ad059a4abcb6406c6d4d043dbf046e31acddecc5
Bashir <b_egeh@hotmail.com>
2020-02-17 13:36:21 -0500

    added useState for setDay and ability to change selected day

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,12 +4,12 @@ import DayListItem from "components/DayListItem.js";
 
 export default function DayList(props) {
   const list = props.days.map((day) =>
-  <DayListItem 
-  key= {day.id}
-  name={day.name} 
-  spots={day.spots} 
-  selected={day.name === props.day}
-  setDay={props.setDay}  />);
+    <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay} />);
   return <ul>
     {list}
   </ul>;
